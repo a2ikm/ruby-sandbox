1e8448c527ccd1f1cac34cae99dbd36acfd87c20
Masato Ikeda <masato.ikeda@gmail.com>
2025-04-25 00:09:29 +0900

    Add test cases for visibility

--- test/module_test.rb
+++ test/module_test.rb
@@ -30,4 +30,40 @@ class TestModule < Test::Unit::TestCase
 
     assert_equal [:mod, :klass], obj.array
   end
+
+  def test_private_method_that_is_prepended_by_public_method_is_still_private
+    mod = Module.new {
+      def hello
+        "Hello"
+      end
+    }
+
+    klass = Class.new {
+      prepend mod
+
+      private def hello
+        "Hello"
+      end
+    }
+
+    assert_equal [:hello], klass.private_instance_methods(false)
+  end
+
+  def test_public_method_that_is_prepended_by_private_method_is_still_public
+    mod = Module.new {
+      private def hello
+        "Hello"
+      end
+    }
+
+    klass = Class.new {
+      prepend mod
+
+      def hello
+        "Hello"
+      end
+    }
+
+    assert_equal [:hello], klass.public_instance_methods(false)
+  end
 end
